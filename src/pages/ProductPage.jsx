12c1b833a17a99734f6798c527cf636725de0c2f
HimeshRoy <himeshroy.cse@gmail.com>
2026-05-05 10:48:02 +0530

    loader timer changed from 2000 to 3000

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -11,7 +11,7 @@ const ProductPage = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 2000);
+    }, 3000);
 
     return () => clearTimeout(timer);
   }, []);
